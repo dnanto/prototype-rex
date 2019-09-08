7179d86536d7dd4a1a7503a1fe98a321197d899d
dnanto <dnegron2@gmu.edu>
2019-09-08 11:46:43 -0400

    adding useful scripts
    to parse date
    to parse subtype from docsum

---
+++ rex/lubridate.R
@@ -0,0 +1,12 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+args <- commandArgs(trailingOnly = T)
+file <- if (args[1] == "-") file("stdin") else args[1]
+cols <- args[2:length(args)]
+
+read_tsv(file("stdin"), col_types = cols(.default = "c")) %>%
+  mutate_at(cols, lubridate::parse_date_time, orders = c("dbY", "Ymd", "bY", "Y"), quiet = T) %>%
+  mutate_at(cols, strftime, format = "%Y-%m-%d") %>%
+  write.table("", row.names = F, quote = F, sep = "\t")
---
+++ rex/subtype.R
@@ -0,0 +1,20 @@
+#!/usr/bin/env Rscript
+
+library(tidyverse)
+
+args <- commandArgs(trailingOnly = T)
+file <- if (length(args) == 0 || args[1] == "-") file("stdin") else args[1]
+
+read_tsv(file, col_names = c("AccessionVersion", "SubType", "SubName")) %>%
+bind_cols(
+  .,
+  bind_rows(
+    apply(., 1, function(row) {
+      key <- str_split(row["SubType"], "\\|")[[1]]
+      val <- str_split(row["SubName"], "\\|")[[1]]
+      data.frame(as.list(setNames(val, key)), stringsAsFactors = F)
+    })
+  )
+) %>%
+select(-SubType, -SubName) %>%
+write.table("", row.names = F, quote = F, sep = "\t")
