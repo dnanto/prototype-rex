fcdbaf515768c84da9187cc9b2302341d98ff66c
dnanto <dnegron2@gmu.edu>
2019-09-23 15:18:12 -0400

    Update ui.R
    tab panels

--- app/ui.R
+++ app/ui.R
@@ -1,33 +1,44 @@
-fluidPage(
-	wellPanel(actionButton("run", "run", width = "100%")),
-	tabsetPanel(
-		tabPanel(
-			"alignment",
-			wellPanel(
-				selectizeInput("db", "db", db$title)
+navbarPage(
+	"rex",
+	tabPanel(
+		"alignment",
+		tabsetPanel(
+			tabPanel(
+				"params",
+				wellPanel(actionButton("run", "run", width = "100%")),
+				wellPanel(
+					shinyFilesButton(
+						"import_qry", "Import FASTA", "Please select a FASTA file...",
+						multiple = F, icon = icon("file-import")
+					),
+					verbatimTextOutput("path_qry", placeholder = T)
+				),
+				DT::DTOutput("query"),
+				wellPanel(
+					selectizeInput("db", "db", db$title)
+				),
+				DT::DTOutput("db")
 			),
-			DT::DTOutput("db"),
-			wellPanel(
-				shinyFilesButton("import_qry", "Import FASTA", "Please select a FASTA file...", multiple = F, icon = icon("file-import")),
-				verbatimTextOutput("path_qry", placeholder = T)
+			tabPanel(
+				"blast",
+				tabsetPanel(
+					tabPanel("hits", DT::DTOutput("tbl.hits.1")),
+					tabPanel("snp", DT::DTOutput("tbl.snps.1")),
+					tabPanel("plot", plotOutput("plt.hits.1", height = 600))
+				)
 			),
-			DT::DTOutput("query")
-		),
-		tabPanel(
-			"hits.1",
-			plotOutput("plt.hits.1", height = 600),
-			DT::DTOutput("tbl.hits.1"),
-			DT::DTOutput("tbl.snps.1")
-		),
-		tabPanel(
-			"hits.2",
-			plotOutput("plt.hits.2", height = 600),
-			DT::DTOutput("tbl.hits.2"),
-			DT::DTOutput("tbl.snps.2")
-		),
-		tabPanel(
-			"extract",
-			DT::DTOutput("extract")
+			tabPanel(
+				"glsearch",
+				tabsetPanel(
+					tabPanel("hits", DT::DTOutput("tbl.hits.2")),
+					tabPanel("snp", DT::DTOutput("tbl.snps.2")),
+					tabPanel("plot", plotOutput("plt.hits.2", height = 600))
+				)
+			),
+			tabPanel(
+				"extract",
+				DT::DTOutput("extract")
+			)
 		)
 	)
 )
